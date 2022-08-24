9a192cba13a4d4ddc3e2d30e356c3a514ed3ef3e
carzso <carzso@carzsos-MacBook-Air.local>
2022-08-24 17:37:53 +0530

    exercise 72 done

--- js/exercises/file72.js
+++ js/exercises/file72.js
@@ -1,3 +1,10 @@
 console.log("File 72 - More about the this")
 
-console.log(this);
+// console.log(this);
+
+function myFunc(){
+    'use strick'
+    console.log("Hello world");
+    // console.log(this);
+}
+myFunc();
